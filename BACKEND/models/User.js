486248f79b5e5d4f4a839a1e6908ba30b663ffe7
Azka azeem <azkaazeem804@gmail.com>
2026-08-26 22:28:41 +0500

    Fix next is not a function error in User pre-save hook

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -38,12 +38,11 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-// SAVE PASSWORD BEFORE HASHING
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+// SAVE PASSWORD BEFORE HASHING (Mongoose modern async middleware does not use next)
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 // PASSWORD COMPARISON METHOD
